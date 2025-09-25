6b1e9a26a5b88d48ccb65d42b96fabf5f8750bb2
Inki <isaiass.10000@gmail.com>
2025-09-24 23:42:25 -0300

    opa

---
+++ script.js
@@ -0,0 +1,28 @@
+// Obtém os elementos de botão e formulários de login/cadastro
+const googleButton = document.getElementById('google');
+const googleLogin = document.getElementById('googleLogin');
+
+
+googleLogin.addEventListener('click', function() {
+    signInWithGoogle(); // Chama a função signInWithGoogle do firebaseauth.js
+});
+// Adiciona um evento de clique ao botão do Google
+googleButton.addEventListener('click', function() {
+    signInWithGoogle(); // Chama a função signInWithGoogle do firebaseauth.js
+});
+const signUpButton = document.getElementById('signUpButton');
+const signInButton = document.getElementById('signInButton');
+const signInForm = document.getElementById('signIn');
+const signUpForm = document.getElementById('signup');
+
+// Quando o botão de cadastro é clicado, esconde o formulário de login e mostra o de cadastro
+signUpButton.addEventListener('click', function() {
+    signInForm.style.display = "none";
+    signUpForm.style.display = "block";
+});
+
+// Quando o botão de login é clicado, esconde o formulário de cadastro e mostra o de login
+signInButton.addEventListener('click', function() {
+    signInForm.style.display = "block";
+    signUpForm.style.display = "none";
+});
